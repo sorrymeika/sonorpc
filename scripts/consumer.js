6b6305fe454aabe61c1c364137b1dde321d28da5
孙路 <>
2019-07-17 10:48:13 +0800

    test args

--- scripts/consumer.js
+++ scripts/consumer.js
@@ -4,4 +4,4 @@ const consumer = registerConsumer({
     port: 3006
 });
 
-consumer.invoke('test.test', []);
+consumer.invoke('test.test', [1, 12345667891234, false, null, undefined, { name: 'asdf' }, 'asdf', ['日']]);
